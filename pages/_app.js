928c8dee8163e8a67dc160bb778e44e101ae2b75
Bader Mohammad <bader97alyami@hotmail.com>
2023-09-28 21:24:48 +0300

    FavIcon was added for SEO

--- pages/_app.js
+++ pages/_app.js
@@ -21,8 +21,19 @@ export default function App({ Component, pageProps }) {
             <meta name="description" content="This is my portfolio website showcasing my work and skills" />
             <meta name="keywords" content="portfolio, web development, design, next.js,animations" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-          
-         
+            {/* The FavIcon */}
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
+            <meta name="theme-color" content="#ffffff" />
+            {/* The FavIcon */}
+            
+
+
             {/*  <!-- GOOGLE SEO Meta Tags --> */}
             <meta name="google-site-verification" content="OQtDBl4loczi5G1TaxIPG9iKNkDrpy0cfg16wleS3Qc" />
 
@@ -39,7 +50,7 @@ export default function App({ Component, pageProps }) {
             <meta name="twitter:title" content="Bader's Mohammad Portfolio" />
             <meta name="twitter:description" content="This is my portfolio website showcasing my work and skills." />
             <meta name="twitter:image" content="https://github.com/Bader975/NEXT-Auth/assets/80196102/d019df5c-5d9d-44c7-bf8d-63f95fb5bb83" />
-           
+
           </Head>
 
           {/* added google fonts */}
