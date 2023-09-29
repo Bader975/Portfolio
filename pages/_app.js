11bfd4b6fc52af33957e9ad77858e4a143dae934
Bader Mohammad <bader97alyami@hotmail.com>
2023-09-29 09:11:20 +0300

    _app.js and Layout was edited

--- pages/_app.js
+++ pages/_app.js
@@ -24,41 +24,9 @@ export default function App({ Component, pageProps }) {
             <meta name="description" content="This is my portfolio website showcasing my work and skills" />
             <meta name="keywords" content="portfolio, web development, design, next.js,animations" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            {/* <!-- Google Tag Manager --> */}
-            {/* <Script>{(function (w, d, s, l, i) {
-              w[l] = w[l] || []; w[l].push({
-                'gtm.start':
-                  new Date().getTime(), event: 'gtm.js'
-              }); var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-                  'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-            })
-            // if(typeof window== "object"){
-              typeof window== "object" (window, document, 'script', 'dataLayer', 'GTM-KVW4KMWP')
-            }
-           */}
-            {/* </Script> */}
-            {/* <!-- End Google Tag Manager --> */}
-            {/* Schema Markup  (JSON-LD)  */}
-            {/* <Script type="application/ld+json">
-              {{
-                "@context": "https://schema.org/",
-                "@type": "CreativeWork",
-                "name": "Bader Mohammad Portfolio ",
-                "alternateName": ["BM", "BADER", "Bader's Portfolio"],
-                "url": "https://www.portfolio-bader.online",
-                "image": "https://www.portfolio-bader.online/bm-logo-remove-bg.png",
-                "jobTitle": "Full Satck Web Developer",
-                "worksFor": {
-                  "@type": "Organization",
-                  "name": "Freelancer"
-                }
-              }
-              }
-            </Script> */}
 
             {/* Schema Markup  (JSON-LD)  GOOGLE */}
-            <script
+            <Script
             type="application/ld+json"
             dangerouslySetInnerHTML={{
               __html: `
