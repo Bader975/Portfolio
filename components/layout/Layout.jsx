0d68d5aff734c98e7b3bb16fa68b272be82d14d3
Bader Mohammad <bader97alyami@hotmail.com>
2023-09-29 09:32:29 +0300

    test Layout was edited

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -4,35 +4,38 @@ import { usePathname } from 'next/navigation';
 import dynamic from 'next/dynamic';
 
 const SplashScreen = dynamic(
-  () => import('../ui/SplashScreen').then((module) => module.default),
-  { ssr: false }
+    () => import('../ui/SplashScreen').then((module) => module.default),
+    { ssr: false }
 );
 
 export default function Layout({ children }) {
-  const pathName = usePathname();
-  const isHome = pathName === "/";
-  const [isLoading, setIsLoading] = useState(isHome);
-  const [showApp, setShowApp] = useState(false);
+    const pathName = usePathname();
+    const isHome = pathName === "/";
+    const [isLoading, setIsLoading] = useState(isHome);
+    const [showApp, setShowApp] = useState(false);
 
-  useEffect(() => {
-    if (!isLoading) {
-      const delay = setTimeout(() => {
-        setShowApp(true);
-      }, 1000);
+    //   useEffect(() => {
+    //     if (!isLoading) {
+    //       const delay = setTimeout(() => {
+    //         setShowApp(true);
+    //       }, 1000);
 
-      return () => clearTimeout(delay);
+    //       return () => clearTimeout(delay);
+    //     }
+    //   }, [isLoading]);
+    useEffect(() => {
+        if (isLoading) return;
+    }, [isLoading])
+
+    // Render the SplashScreen component on the server-side
+    if (isLoading && isHome) {
+        return <SplashScreen endLoading={() => setIsLoading(false)} />;
     }
-  }, [isLoading]);
 
-  // Render the SplashScreen component on the server-side
-  if (isLoading && isHome) {
-    return <SplashScreen endLoading={() => setIsLoading(false)} />;
-  }
-
-  return (
-    <>
-      <NavBar />
-      {showApp && <>{children}</>}
-    </>
-  );
+    return (
+        <>
+            <NavBar />
+            {showApp && <>{children}</>}
+        </>
+    );
 }
