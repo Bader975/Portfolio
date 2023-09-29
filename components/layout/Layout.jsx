65a2efa6ee3ad79e278598b4585560c356209b14
Bader Mohammad <bader97alyami@hotmail.com>
2023-09-29 09:45:15 +0300

    Layout was edited for 10 times

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,34 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import NavBar from './NavBar';
 import { usePathname } from 'next/navigation';
-import dynamic from 'next/dynamic';
-
-const SplashScreen = dynamic(
-    () => import('../ui/SplashScreen').then((module) => module.default),
-    { ssr: false }
-);
+import SplashScreen from '../ui/SplashScreen';
 
 export default function Layout({ children }) {
-    const pathName = usePathname();
-    const isHome = pathName === "/";
-    const [isLoading, setIsLoading] = useState(isHome);
+  const pathName = usePathname();
+  const isHome = pathName === '/';
+  const [isLoading, setIsLoading] = useState(isHome);
 
+  useEffect(() => {
+    if (isLoading) return;
+}, [isLoading])
 
-    useEffect(() => {
-        if (isLoading) return;
-    }, [isLoading])
-
-    // Render the SplashScreen component on the server-side
-    if (isLoading && isHome) {
-        return <SplashScreen endLoading={() => setIsLoading(false)} />;
-    }
-
-    return (
+  return (
+    <>
+      {isLoading && isHome ? 
+        <div>
+            <SplashScreen endLoading={() => setIsLoading(false)} />
+        </div>
+       : (
         <>
-            <NavBar />
-          <>
-          {children}
-          </>
+          <NavBar />
+          <>{children}</>
         </>
-    );
+      )}
+    </>
+  );
 }
