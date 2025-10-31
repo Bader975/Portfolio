db357bf0063f3940edc615f555881d81b3593b35
Bader  Alyami <80196102+Bader975@users.noreply.github.com>
2025-10-31 22:40:01 +0300

    geniral improvment

--- data/projects.js
+++ data/projects.js
@@ -1,8 +1,17 @@
 
 export const projectsArray = [
 
-    {
+     {
         id: 1,
+        title: "كــــدان - Kidan", 
+        discription: "في كدان نمكّنك من تحقيق أعلى مستويات الكفاءة، الشفافية، والاستدامة عبر حلول متكاملة صممت خصيصاً للقطاع الثالث",
+        href: "https://new.kidanm.com.sa/",
+        image: "/projecs-images/kedan.png"
+       
+    },
+
+    {
+        id: 2,
         title: "Wajeeh Bot ",
         discription: "المساعد الافتراضي الأول في عالم التعليم بالتكامل مع مسارات التعلم المرن",
         href: "https://wajeeh-snowy.vercel.app/",
@@ -10,21 +19,21 @@ export const projectsArray = [
        
     },
     {
-        id: 10,
+        id: 3,
         title: "common voice Arabic Version (الجداريــة الصوتيـة)",
         discription: " Arabic voice data controller, which participates in recording Arabic voice calls and interacts with a distinctive mark.",
         href: "https://falak.ksaa.gov.sa/voicewall/ar",
         image: "/projecs-images/common-voice-ar.png"
     },
     {
-        id: 2,
+        id: 4,
         title: "Invest With Us",
         discription: "An investment platform for informed decision-making and linking between the invester and thecompany ",
         href: "https://react-app-b699b.firebaseapp.com/",
         image: "/projecs-images/invest-with-us.webp"
     },
     {
-        id: 4,
+        id: 5,
         title: "Tuwaiq Projects",
         discription: " Website used (React.TS - Node.js - Express.js) helps Tuwaiq students to get project ideas provided by companies, upload their projects, and display them for any company. They can also modify their projects if they wish.",
         href: "https://github.com/Bader975/Tuwaiq-Projects",
@@ -32,7 +41,7 @@ export const projectsArray = [
     },
 
     {
-        id: 5,
+        id: 6,
         title: "Aramco Direct Hires Dashboard",
         discription: "Dashboard by (HTML-Bootstrp-Jquery-chart.js) that streamlines the direct hiring process at Aramco, enhancing and improving communication between candidates and recruiters",
         href: "https://bader10alyami.000webhostapp.com/Aramco%20Pages/Dashboard/index.html",
@@ -40,7 +49,7 @@ export const projectsArray = [
     },
 
     {
-        id: 6,
+        id: 7,
         title: "Aramco Home Page",
         discription: " A visually appealing and user-friendly home page for Aramco, showcasing their brand and providing easy access to essential information.",
         href: "https://bader10alyami.000webhostapp.com/Aramco%20Pages/Aramco%20Home%20Page/aramco%20page%20.html",
@@ -48,7 +57,7 @@ export const projectsArray = [
     },
 
     {
-        id: 7,
+        id: 8,
         title: "Aramco VPs PDF viewer",
         discription: "A PDF viewer using JSpdf specifically designed for Aramco's Vice Presidents, ensuring a smooth and secure document viewing experience.",
         href: "https://bader10alyami.000webhostapp.com/Aramco%20Pages/PDF%20Viewer/pdf.html",
@@ -56,21 +65,21 @@ export const projectsArray = [
     },
 
     {
-        id: 8,
+        id: 9,
         title: "RATEME.com",
         discription: "Our site offers a rating of local restaurants and hotels in order to search for the best with ease.",
         href: "https://bader10alyami.000webhostapp.com/Ratings/restaurantsHome.html",
         image: "/projecs-images/rateme.webp"
     },
     {
-        id: 3,
+        id: 10,
         title: "NEXT.JS App MeetUp",
         discription: "Web app that uses NEXT.JS and Mongodb to add and manage your meetups and put the important in favorites meetup",
         href: "https://github.com/Bader975/nextjs-meetup/tree/main",
         image: "/projecs-images/nextjs-meetup.webp"
     },
     {
-        id: 9,
+        id: 11,
         title: "NEXT.JS Auth ",
         discription: "Used NextAuth to implement login and logout operations and sessions",
         href: "https://github.com/Bader975/NEXT-Auth",
