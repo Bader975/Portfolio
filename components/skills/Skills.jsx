9f8cf4b271ace52ca100d629c01cd0c509a69adb
Bader  Alyami <80196102+Bader975@users.noreply.github.com>
2026-02-24 01:57:28 +0300

    refactor: move skills array definition outside of the Skills component function

--- components/skills/Skills.jsx
+++ components/skills/Skills.jsx
@@ -29,31 +29,30 @@ import {
 } from "react-icons/si";
 import SkillCard from "./SkillCard";
 
+const skills = [
+  { icon: <FaHtml5 color="#E34F26" />, label: "HTML" },
+  { icon: <FaCss3 color="#2965F1" />, label: "CSS" },
+  { icon: <SiTailwindcss color="#38bdf9" />, label: "Tailwind CSS" },
+  { icon: <FaBootstrap color="#7952B3" />, label: "Bootstrap" },
+  { icon: <SiChakraui color="#319795" />, label: "Chakra UI" },
+  { icon: <FaAngular />, label: "Angular Material" },
+  { icon: <FaChartBar color="#FF6384" />, label: "Chart.js" },
+  { icon: <SiJquery color="#0868AC" />, label: "jQuery" },
+  { icon: <FaReact color="#61DAFB" />, label: "React.js" },
+  { icon: <FaAngular color="#c42d52" />, label: "Angular" },
+  { icon: <SiNextdotjs color="#000000" />, label: "Next.js" },
+  { icon: <SiJavascript color="#F7DF1E" />, label: "JavaScript" },
+  { icon: <SiTypescript color="#3178C6" />, label: "TypeScript" },
+  { icon: <FaNodeJs color="#339933" />, label: "Node.js" },
+  { icon: <SiExpress color="#000000" />, label: "Express.js" },
+  { icon: <FaDatabase color="#4479A1" />, label: "MySQL" },
+  { icon: <SiPrisma color="#1B222D" />, label: "Prisma" },
+  { icon: <FaGit color="#F05032" />, label: "Git" },
+];
+
 export default function Skills() {
   const iconSize = useBreakpointValue({ base: 30, md: 50 });
   const boxSize = useBreakpointValue({ base: 4, md: 5 });
-
-
-  const skills = [
-    { icon: <FaHtml5 color="#E34F26" />, label: "HTML" },
-    { icon: <FaCss3 color="#2965F1" />, label: "CSS" },
-    { icon: <SiTailwindcss color="#38bdf9" />, label: "Tailwind CSS" },
-    { icon: <FaBootstrap color="#7952B3" />, label: "Bootstrap" },
-    { icon: <SiChakraui color="#319795" />, label: "Chakra UI" },
-    { icon: <FaAngular />, label: "Angular Material" },
-    { icon: <FaChartBar color="#FF6384" />, label: "Chart.js" },
-    { icon: <SiJquery color="#0868AC" />, label: "jQuery" },
-    { icon: <FaReact color="#61DAFB" />, label: "React.js" },
-    { icon: <FaAngular color="#c42d52" />, label: "Angular" },
-    { icon: <SiNextdotjs color="#000000" />, label: "Next.js" },
-    { icon: <SiJavascript color="#F7DF1E" />, label: "JavaScript" },
-    { icon: <SiTypescript color="#3178C6" />, label: "TypeScript" },
-    { icon: <FaNodeJs color="#339933" />, label: "Node.js" },
-    { icon: <SiExpress color="#000000" />, label: "Express.js" },
-    { icon: <FaDatabase color="#4479A1" />, label: "MySQL" },
-    { icon: <SiPrisma color="#1B222D" />, label: "Prisma" },
-    { icon: <FaGit color="#F05032" />, label: "Git" },
-  ];
 
   return (
     <Box
